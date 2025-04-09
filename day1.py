86f30b220339c8826a83be8f7566ee34ff46abb5
DiegoVIeiras <diegow.contato@gmail.com>
2025-04-09 09:17:38 -0300

    new file test

---
+++ day1.py
@@ -0,0 +1,11 @@
+name = "Diego"
+idade = 10
+maior_de_idade = idade >= 18
+
+if maior_de_idade == True:
+    print("Nome: ",name,"Idade: ",idade,"\nEle é maior de idade")
+else:
+    print("Nome: ",name,"Idade: ",idade,"\nEle é menor de idade")
+
+
+
